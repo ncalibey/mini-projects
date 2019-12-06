0c71e47e1384b4b50462304b215a02438d1ec379
Nick Calibey <nick.calibey@gmail.com>
2019-12-05 22:44:43 -0600

    Add wonderful-quotes Vue app

---
+++ javascript/vue/wonderful-quotes/README.md
@@ -0,0 +1,16 @@
+wonderful quotes
+===
+This is a small VueJS application that allows a user to add up to 10 quotes that they like. Clicking on a note will remove it. This app was largely built to practice using slots and emitting events. It's part of Maximilian Schwarzmüller's _[Vue JS 2 - The Complete Guide](https://www.udemy.com/course/vuejs-2-the-complete-guide/)_ Udemy course.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
---
+++ javascript/vue/wonderful-quotes/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="addQuote($event)"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote($event)"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to Delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+
+    export default {
+        data() {
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10,
+            };
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header,
+        },
+        methods: {
+            addQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete quotes first!');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(i) {
+                this.quotes.splice(i, 1);
+            },
+        },
+    }
+</script>
+
+<style>
+</style>
