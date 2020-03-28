0c7750061ab711a5a54edbb762789e06db74f471
Nick Calibey <nick.calibey@gmail.com>
2020-03-28 11:59:52 -0500

    Add flutter xylophone app

---
+++ flutter/xylophone-app/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final player = AudioCache();
+    void playAudio(int soundNumber) {
+      player.play('note$soundNumber.wav');
+    }
+
+    Expanded buildKey({color: Colors, soundNumber: int}) {
+      return Expanded(
+        child: FlatButton(
+          color: color,
+          onPressed: () {
+            playAudio(soundNumber);
+          },
+          child: null,
+        ),
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.blue, soundNumber: 5),
+              buildKey(color: Colors.indigo, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
