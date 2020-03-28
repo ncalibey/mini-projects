3c79e70c2a78fe0b05d92daa43d0ac577965340d
Nick Calibey <nick.calibey@gmail.com>
2020-03-28 12:01:07 -0500

    Add quizzler app (flutter)

---
+++ flutter/quizzler/lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  int questionNumber = 0;
+
+  void updateScoreKeeper(Icon icon) {
+    scoreKeeper.add(icon);
+  }
+
+  void resetQuiz() {
+    Alert(
+            context: context,
+            title: "FINISHED",
+            desc: "Thanks for taking our quiz!")
+        .show();
+    quizBrain.reset();
+    scoreKeeper = [];
+  }
+
+  void checkAnswer(bool answer) {
+    if (quizBrain.isFinished()) {
+      resetQuiz();
+    } else {
+      if (quizBrain.getQuestionAnswer() == answer) {
+        updateScoreKeeper(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      } else {
+        updateScoreKeeper(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  checkAnswer(true);
+                  quizBrain.nextQuestion();
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  checkAnswer(false);
+                  quizBrain.nextQuestion();
+                });
+              },
+            ),
+          ),
+        ),
+        Row(children: scoreKeeper)
+      ],
+    );
+  }
+}
